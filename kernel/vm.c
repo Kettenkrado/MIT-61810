a8dbcf1c9f32ec3bf0468df70860bd8a522da62a
Kettenkrad <kettenkrad@Kettenkrad-2.local>
2025-09-10 08:01:55 +0800

    lab3: Print a page table completed

--- kernel/vm.c
+++ kernel/vm.c
@@ -488,9 +488,31 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
 
 
 #ifdef LAB_PGTBL
+void 
+vmprint_helper(pagetable_t pagetable, uint64 va_base, int level) {
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if (pte & PTE_V) {
+      uint64 pa = PTE2PA(pte);
+      uint64 va = (va_base | i) << 30;
+
+      for (int j = 0; j < level; j++) {
+        printf(" ..");
+        va >>= 9;
+      }
+      printf(" ..%p: pte %p pa %p\n", (void *)va, (void *)pte, (void *)pa);
+
+      if ((pte & (PTE_R | PTE_W | PTE_X)) == 0) {
+        vmprint_helper((pagetable_t)pa, (va_base | i) << 9, level + 1);
+      }
+    }
+  }
+}
+
 void
 vmprint(pagetable_t pagetable) {
-  // your code here
+  printf("page table %p\n", pagetable);
+  vmprint_helper(pagetable, 0, 0);
 }
 #endif
 
