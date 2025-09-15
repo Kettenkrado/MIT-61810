ca186e4fd9000584491a924f6b420c31e2cbe5c6
Kettenkrad <kettenkrad@Kettenkrad-2.local>
2025-09-15 20:37:13 +0800

    lab4: minor changes on fork

--- kernel/proc.c
+++ kernel/proc.c
@@ -314,6 +314,9 @@ fork(void)
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
 
+  // copy alarm trapframe
+  *(np->alarm_trapframe) = *(p->alarm_trapframe);
+
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
 
